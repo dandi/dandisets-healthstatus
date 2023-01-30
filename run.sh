bed692f090b05f568398ed95446b24c40dafa83b
John T. Wodder II <git@varonathe.org>
2023-01-30 11:12:33 -0500

    Support taking a list of Dandiset IDs to check as command-line arguments

--- run.sh
+++ run.sh
@@ -14,7 +14,7 @@ git pull
 "$PYTHON" -m virtualenv --clear venv
 . venv/bin/activate
 pip install -q -r code/requirements.txt
-python code/healthstatus.py -d "$DANDISETS_PATH" -m "$MOUNT_PATH"
+python code/healthstatus.py -d "$DANDISETS_PATH" -m "$MOUNT_PATH" "$@"
 
 git add .
 if ! git diff --quiet --cached
