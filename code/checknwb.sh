8766476f62df6507eaed356d4f5299540245b2a8
DANDI Team <team@dandiarchive.org>
2023-02-02 14:18:31 -0500

    Apparently we need to point MATLABPATH also to the "savedir"
    ref: https://github.com/NeurodataWithoutBorders/matnwb/issues/481#issuecomment-1414200403

--- code/checknwb.sh
+++ code/checknwb.sh
@@ -28,7 +28,7 @@ for v in "$@"; do
 	git checkout $v; 
 	echo "Running using: "; git describe --tags; 
 	matlab -nodesktop -sd $PWD -batch 'generateCore()'; 
-	MATLABPATH=$PWD time matlab -nodesktop -batch "nwb = nwbRead('../$ds/$f', 'savedir', '../out')" || echo "Exited with $?"
+	MATLABPATH=$PWD:$PWD/../out time matlab -nodesktop -batch "nwb = nwbRead('../$ds/$f', 'savedir', '../out')" || echo "Exited with $?"
 done
 
 pwd
