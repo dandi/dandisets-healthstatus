79e6b2ad487fde319c94f223ce7c365cb98da5a8
DANDI Team <team@dandiarchive.org>
2023-02-02 14:13:08 -0500

    Add a little self-sufficient helper  to check matnwb on some dandiset asset

---
+++ code/checknwb.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -eu
+
+cd "$(mktemp -d ${TMPDIR:-/tmp}/matnwb-XXXXXXX)"
+pwd
+
+ds="$1"
+f="$2"
+
+shift 
+shift
+
+git clone https://github.com/dandisets/$ds
+( cd $ds; 
+  echo "Version of the dandiset: ";
+  git describe --always
+  git annex get "$f"
+)
+
+mkdir out
+
+git clone https://github.com/NeurodataWithoutBorders/matnwb
+cd matnwb
+
+set -x
+for v in "$@"; do 
+	git checkout $v; 
+	echo "Running using: "; git describe --tags; 
+	matlab -nodesktop -sd $PWD -batch 'generateCore()'; 
+	MATLABPATH=$PWD time matlab -nodesktop -batch "nwb = nwbRead('../$ds/$f', 'savedir', '../out')" || echo "Exited with $?"
+done
+
+pwd
