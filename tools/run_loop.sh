aa816f0601672e09add88c7fcc64cda942576c52
DANDI Team <team@dandiarchive.org>
2024-03-08 21:20:50 -0500

    loop even if errors out

--- tools/run_loop.sh
+++ tools/run_loop.sh
@@ -5,5 +5,5 @@ set -eu;
 cd $(dirname $0)
 
 while true; do
-	chronic ./run.sh --mode random-outdated-asset-first;
+	chronic ./run.sh --mode random-outdated-asset-first || { echo "run exited with $?. Sleeping some and going for the next round"; sleep 600; }
 done;
