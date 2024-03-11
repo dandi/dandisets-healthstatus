4192e0cddfbd863099b8ef632b9d51011f93bd5e
DANDI Team <team@dandiarchive.org>
2024-03-11 09:46:30 -0400

    Results from a sweep of an archive: --mode random-outdated-asset-first

--- tools/run_loop.sh
+++ tools/run_loop.sh
@@ -5,5 +5,6 @@ set -eu;
 cd $(dirname $0)
 
 while true; do
+	echo "Let's do it"
 	chronic ./run.sh --mode random-outdated-asset-first || { echo "run exited with $?. Sleeping some and going for the next round"; sleep 600; }
 done;
