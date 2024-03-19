9d7bc9381683fdff1578009d9e6dacbd3f97d3a6
DANDI Team <team@dandiarchive.org>
2024-03-19 19:57:23 -0400

    Use recent datalad-fuse without caching by default (yet to be released) + disable custom patched fsspec

--- tools/run.sh
+++ tools/run.sh
@@ -17,7 +17,7 @@ cd "$(dirname "$0")"/..
 . venv/bin/activate
 pip install ./code
 #pip install 'git+https://github.com/fsspec/filesystem_spec'
-pip install 'git+https://github.com/jwodder/filesystem_spec@rlock-cache'
+#pip install 'git+https://github.com/jwodder/filesystem_spec@rlock-cache'
 dandisets-healthstatus check -d "$DANDISETS_PATH" -m "$MOUNT_PATH" -J 10 "$@"
 dandisets-healthstatus report
 
