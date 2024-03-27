9a13d6e239a1213af3819813609ea22d939b63ac
John T. Wodder II <git@varonathe.org>
2024-03-27 10:04:12 -0400

    Shell script for running benchmarks

---
+++ tools/bench.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -ex
+
+PYTHON="$HOME"/miniconda3/bin/python
+DANDISETS_PATH=/mnt/backup/dandi/dandisets-healthstatus/dandisets
+MOUNT_PATH=/tmp/dandisets-fuse
+
+cd "$(dirname "$0")"/..
+
+"$PYTHON" -m virtualenv --clear venv
+. venv/bin/activate
+pip install './code[dandi]'
+pip install 'git+https://github.com/jwodder/filesystem_spec@rlock-cache'
+
+dandisets-healthstatus time-mounts \
+    -d "$DANDISETS_PATH" \
+    -m "$MOUNT_PATH" \
+    --no-update-dataset \
+    --mounts fusefs,davfs2 \
+    000016/sub-mouse1-fni16/sub-mouse1-fni16_ses-161228151100.nwb
